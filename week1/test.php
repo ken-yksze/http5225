702798a583c3cb6f5781c953341d9295d2ec1ca5
ken-yksze <ken.yksze@gmail.com>
2025-01-14 20:17:30 -0500

    week1

---
+++ week1/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro to PHP</title>
+</head>
+
+<body>
+    <h1>Intro to PHP</h1>
+    <?php
+    date_default_timezone_set('EST');
+    echo date("l F j, Y");
+    ?>
+</body>
+
+</html>
